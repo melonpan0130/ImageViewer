5a74e6594ff2ebe8d982bcc579b1ef06fbd34150
Eunju Chong <eun01300714@gmail.com>
2019-06-12 11:56:07 +0900

    #1 design form

--- ImageViewer/Form1.cs
+++ ImageViewer/Form1.cs
@@ -12,9 +12,41 @@ namespace ImageViewer
 {
     public partial class Form1 : Form
     {
+        int imgCount=0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowImage();
+        }
+
+        private void ShowImage()
+        {
+            this.picImg.Image = this.imgList.Images[this.imgCount];
+            this.lblNum.Text = (this.imgCount + 1) + " / " + this.imgList.Images.Count;
+        }
+
+        private void BtnPrev_Click(object sender, EventArgs e)
+        {
+            this.imgCount--;
+            if (this.imgCount < 0)
+            {
+                this.imgCount = this.imgList.Images.Count - 1;
+            }
+            ShowImage();
+        }
+
+        private void BtnNext_Click(object sender, EventArgs e)
+        {
+            this.imgCount++;
+            if (this.imgCount >= this.imgList.Images.Count)
+            {
+                this.imgCount = 0;
+            }
+            ShowImage();
         }
     }
 }
